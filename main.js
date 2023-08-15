726d6f8f0972ab85a12cf9ab4c6ad24189367a3c
Lucas santos <Lucass.santos2015@outlook.com>
2023-08-15 20:44:55 -0300

    realizando jquery e suas funcoes

---
+++ main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    console.log(document.querySelector('header button'));
+    console.log($('#botao-cancelar'));
+})
+
+document.querySelector('header button').addEventListener("click", function() {
+
+})
+
+$('header button').click(function() {
+    alert("Expandir formulario")
+})
+})
