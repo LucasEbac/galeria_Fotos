58a2ce45cf006414fd128f35506cb6c0b7de6c74
Lucas santos <Lucass.santos2015@outlook.com>
2023-08-21 20:25:35 -0300

    front finalizado

--- main.js
+++ main.js
@@ -8,7 +8,18 @@ $('#botao-cancelar').click(function() {
     $('form').slideUp();
 })
 $('form').on("submit", function(e){
-    console.log("submit");
     e.preventDefault();
+    const enderecoNovaimagem = $('endereco-imagem-nova').val();
+    const novoItem = $('<li><li/>');
+    $(`<img src="${enderecoNovaimagem}" />`).appendTo(novoItem);
+    $(`
+            <div class="overlay-imagem-link"> 
+            <a href="${enderecoNovaimagem}" target="blank" title="ver imagem em tamanho real"   
+            ver imagem em tamanho real 
+            </a>
+            </div>
+    `).appendTo(novoItem);
+    $(novoItem).appendTo('ul');
+    $('endereco-imagem-nova').val('')
 })
 })
