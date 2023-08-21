3c15e6039d3b726141df12ae38a563ee8e7daaf5
Lucas santos <Lucass.santos2015@outlook.com>
2023-08-21 15:52:43 -0300

    atualizando front

--- main.js
+++ main.js
@@ -1,16 +1,11 @@
 $(document).ready(function(){
-    console.log(document.querySelector('header button'));
-    console.log($('#botao-cancelar'));
-
-
-document.querySelector('header button').addEventListener("click", function() {
-    $('#botao-cancelar').click(function() {
-        $('form').slideUp();
+    $('header button').click(function() {
+        $('form').slideDown();
 
 })
 
-$('header button').click(function() {
-    $('form').slideDown();
+$('#botao-cancelar').click(function() {
+    $('form').slideUp();
 })
 $('form').on("submit", function(e){
     console.log("submit");
